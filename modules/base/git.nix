d138c5730c711b6a63f8ffb0cf117699d7d4baaf
ozozka <ozkaya.ogzhn@gmail.com>
2026-09-21 12:10:26 +0300

    move gh to git and change email

--- modules/base/git.nix
+++ modules/base/git.nix
@@ -1,17 +1,27 @@
-_: {
-  environment.shellAliases = {
-    gs = "git status --short --branch";
-    ga = "git add";
-    gc = "git commit";
-    gp = "git push";
-    gpu = "git pull";
-    gd = "git diff";
-    gdt = "git difftool";
-    gl = "git log --oneline -6";
-    gi = "git diff --stat";
-    gb = "git branch";
-    gch = "git checkout";
-    gm = "git merge";
+{ pkgs, ... }:
+
+{
+  environment = {
+    systemPackages = with pkgs; [ gh ];
+
+    sessionVariables = {
+      GH_TELEMETRY = "0";
+    };
+
+    shellAliases = {
+      gs = "git status --short --branch";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+      gpu = "git pull";
+      gd = "git diff";
+      gdt = "git difftool";
+      gl = "git log --oneline -6";
+      gi = "git diff --stat";
+      gb = "git branch";
+      gch = "git checkout";
+      gm = "git merge";
+    };
   };
 
   programs.git = {
@@ -19,10 +29,10 @@ _: {
     config = {
       init.defaultBranch = "main";
       pull.rebase = true;
-      safe.directory = "/etc/nixos";
+      push.autoSetupRemote = true;
       user = {
         name = "ozozka";
-        email = "ozkayaoguzhan67@gmail.com";
+        email = "ozkaya.ogzhn@gmail.com";
       };
 
       diff.tool = "nvimdiff";
