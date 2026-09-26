a60d300c9c4d129d092b7116ef558cc23fbdc4e6
ozozka <ozkaya.ogzhn@gmail.com>
2026-09-26 19:58:16 +0300

    remove linguist from detailed loc module commands

--- modules/misc/loc.nix
+++ modules/misc/loc.nix
@@ -9,8 +9,8 @@
 
     shellAliases = {
       loc = "scc -s lines --no-size --no-cocomo && github-linguist";
-      locd = "scc -s lines -a -p --sloccount-format && github-linguist";
-      locf = "scc -s lines -a -p --sloccount-format --by-file && github-linguist";
+      locd = "scc -s lines -a -p --sloccount-format";
+      locf = "scc -s lines -a -p --sloccount-format --by-file";
     };
   };
 }
